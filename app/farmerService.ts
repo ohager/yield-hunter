f434a8d44e9661e8fda17a43aafc8fbd893c43d4
ohager <oliver.hager@dextra-sw.com>
2020-12-09 20:38:41 -0300

    chore: removed rtk

--- app/farmerService.ts
+++ app/farmerService.ts
@@ -1,6 +1,17 @@
 import {Api} from "@burstjs/core";
 import {FarmLandData} from "../types/FarmLandData";
 
+const LordMethodHashes = {
+    stake: '-2656511522688860312',
+    buyIn: '5623382620658472920',
+    buyOut: '3920936425848631052',
+    takeOver: '-5414711536059530086'
+}
+
+const FarmerMethodHashes = {
+    payout: '-4576041685933966606'
+}
+
 export class FarmerService {
     constructor(private burstApi: Api, private contractIds: string[]) {
     }
